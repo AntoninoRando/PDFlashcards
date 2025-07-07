7e6b630fc0dc699ffbd2a5efa9d615271b2005fe
AntoninoRando <antonino.rando.2003@gmail.com>
2025-07-07 13:28:00 +0200

    feat: Add visual effect on recall option select

--- src/components/Flashcards/Flashcard.vue
+++ src/components/Flashcards/Flashcard.vue
@@ -9,25 +9,29 @@
                 {{ flashcard.frontText }}
             </button>
         </div>
-        <div v-else class="buttons-container">
-            <button class="revealed-button" style="border-radius: 7px 0px 0px 7px;" @click="hide">Hide</button>
-            <button class="revealed-button" @click="forgot">Forgot</button>
-            <button class="revealed-button" @click="bad">Bad</button>
-            <button class="revealed-button" @click="notBad">Fine</button>
-            <button class="revealed-button" style="border-radius: 0px 7px 7px 0px;" @click="ok">Ok</button>
+        <div v-else class="buttons-container" :class="{ 'hiding': isHiding }">
+            <button class="revealed-button" :class="{ 'pressed': pressedButton === 'hide' }"
+                style="border-radius: 7px 0px 0px 7px;" @click="hide">Hide</button>
+            <button class="revealed-button" :class="{ 'pressed': pressedButton === 'forgot' }"
+                @click="forgot">Forgot</button>
+            <button class="revealed-button" :class="{ 'pressed': pressedButton === 'bad' }" @click="bad">Bad</button>
+            <button class="revealed-button" :class="{ 'pressed': pressedButton === 'fine' }"
+                @click="notBad">Fine</button>
+            <button class="revealed-button" :class="{ 'pressed': pressedButton === 'ok' }"
+                style="border-radius: 0px 7px 7px 0px; border-width: 1px 1px 1px 1px;" @click="ok">Ok</button>
         </div>
     </div>
 </template>
-
 <script lang="ts">
-
 export default {
     emits: ['reveal', 'hide'],
     props: ['flashcard'],
     expose: ['isRevealed', 'reveal', 'hide', 'forgot', 'bad', 'notBad', 'ok'],
     data() {
         return {
-            revealed: false
+            revealed: false,
+            isHiding: false,
+            pressedButton: null
         }
     },
     computed: {
@@ -48,27 +52,39 @@ export default {
             this.revealed = true
             this.$emit('reveal', this.flashcard)
         },
-        hide() {
-            this.revealed = false
-            this.flashcard.reviewedAt = new Date()
-            this.$emit('hide', this.flashcard)
+        hide(fromAction = false) {
+            if (!fromAction) {
+                this.pressedButton = 'hide'
+            }
+            this.isHiding = true
+            // Wait for animation to complete before hiding
+            setTimeout(() => {
+                this.revealed = false
+                this.isHiding = false
+                this.pressedButton = null
+                this.flashcard.reviewedAt = new Date()
+                this.$emit('hide', this.flashcard)
+            }, 300) // Match the CSS animation duration
         },
         forgot() {
+            this.pressedButton = 'forgot'
             this.hide(true)
         },
         bad() {
+            this.pressedButton = 'bad'
             this.hide(true)
         },
         notBad() {
+            this.pressedButton = 'fine'
             this.hide(true)
         },
         ok() {
+            this.pressedButton = 'ok'
             this.hide(true)
         }
     },
 }
 </script>
-
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap');
 
@@ -88,6 +104,14 @@ export default {
 .buttons-container {
     height: 100%;
     width: 100%;
+    transition: all 0.3s ease-in-out;
+    transform: scale(1);
+    opacity: 1;
+}
+
+.buttons-container.hiding {
+    transform: scale(0.95);
+    opacity: 0;
 }
 
 .flashcard-button {
@@ -118,6 +142,22 @@ export default {
     width: 20%;
     background-color: rgba(0, 0, 0, 0.05);
     border-color: rgba(0, 0, 0, 0.3);
-    border-width: 1px;
+    border-width: 1px 0px 1px 1px;
+    transition: all 0.2s ease;
+}
+
+.revealed-button:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+    transform: translateY(-1px);
+}
+
+.revealed-button.pressed {
+    background-color: #4CAF50;
+    color: white;
+    border-color: #45a049;
+    transform: scale(1.05);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    z-index: 10;
+    position: relative;
 }
 </style>
